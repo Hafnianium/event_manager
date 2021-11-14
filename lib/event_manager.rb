d1c99a3f6ff5570d12b91d0281867363aad669db
patrick <patrick@o-neill.ca>
2021-11-14 12:18:46 -0500

    removed redundant csv open statement

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -39,12 +39,6 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
-contents = CSV.open(
-  'event_attendees.csv',
-  headers: true,
-  header_converters: :symbol
-)
-
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
